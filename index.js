5e4352e36560c26b6c978564c8da021bf959e778
acazorla72@gmail.com <acazorla72@gmail.com>
2021-04-12 07:40:10 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let word = 'miami';
+let arr = ['bcn', 'mia', 'sao', 'mex', 'par', 'miami', 'ams', 'ber', 'paris', 'lis', 'mad'];
+
+for(var i = 0; i < arr.length; i++) {
+  if (word === arr[i] ) {
+    console.log(word);
+    break;
+  } else {
+    console.log("Not Match Found");
+  }
+}
+
+const prices = [10.99, 44.56, 112.79, 3, 5];
+let sum = 0; // start value for the sum
+
+for(var i = 0; i < prices.length; i++) {
+  sum += prices[i]
+} console.log(sum); 
+
+let rounded = Math.round(sum * 10) / 10;
+console.log(rounded);
