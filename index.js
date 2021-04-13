61d6d8357e6602a29bad7006b60dd0bab3d59b61
acazorla72@gmail.com <acazorla72@gmail.com>
2021-04-13 07:53:16 +0200

    Nested Objects

--- index.js
+++ index.js
@@ -41,3 +41,90 @@ let student = {
   } for (let key in product) {
     product.price = product.price * 0.9;
   } console.log(product);
+
+  let products = [
+    {
+      name: 'iPhone',
+      price: 799.99
+    },
+    {
+      name: 'Samsung Galaxy S10',
+      price: 900.0
+    }
+  ];
+  console.log(products[0].name);
+  console.log(products[1].name);
+  
+  
+  let course = {
+    name: 'Web Development',
+    type: ['full-time', 'part-time'],
+    topics: ['HTML/CSS & Responsive Design', 'JavaScript', 'MongoDB', 'Node', 'Express', 'React']
+  };
+  console.log(course.type[1]);
+  console.log(course.topics[0]);
+  console.log(course.topics[4]);
+  
+  let student = {
+    firstName: 'Ana',
+    lastName: 'Blair',
+    course: {
+      name: 'Web Development',
+      type: 'part-time'
+    },
+    attendedIn: 'Madrid',
+    address: {
+      street: 'Happy Street',
+      number: 123,
+      city: 'Barcelona',
+      zip: 08015,
+      country: 'Spain'
+    }
+  };
+  
+  console.log(student.course.name);
+  console.log(student.address.street);
+  console.log(`Ana moved from ${student.address.city} to ${student.attendIn} to take the ${student.type} ${student.course.name} course`);
+  
+  const ironCampuses = [
+    ["Mexico City", "Miami", "Sao Paulo"],
+    ["Amsterdam", "Barcelona", "Berlin", "Lisbon", "Madrid", "Paris"]
+  ];
+  
+  console.log(ironCampuses[0][1]); // => Miami
+  console.log(ironCampuses[1][0]); // => Amsterdam
+  console.log(ironCampuses[1][5]); // => Paris
+
+  let basic = {
+    language: 'JavaScript',
+    frameworks: [
+      {
+        end: 'back',
+        list: [
+          {
+            name: 'ExpressJS',
+            released: 2010
+          },
+          {
+            name: 'MeteorJS',
+            released: 2012
+          }
+        ]
+      },
+      {
+        end: 'front',
+        list: [
+          {
+            name: 'ReactJS',
+            released: 2013
+          },
+          {
+            name: 'VueJS',
+            released: 2014
+          }
+        ]
+      }
+    ]
+  };
+  console.log(basic.frameworks[0].list[0].name); // => ExpressJS
+  console.log(`In Ironhack, I'll learn ${basic.frameworks[0].list[0].name} and ${basic.frameworks[1].list[0].name}.`);  // => In Ironhack, I'll learn ExpressJS and ReactJS.
