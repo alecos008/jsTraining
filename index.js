2587a27aed0dce0a216e80a198b6cd643a87273d
acazorla72@gmail.com <acazorla72@gmail.com>
2021-05-02 12:28:32 +0200

    added checkProperty function

--- index.js
+++ index.js
@@ -261,5 +261,33 @@ function getCredentials (obj) {
 }
 
 getCredentials (user);
+
+let property = {
+  owner: {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 44
+  },
+  isForSale: true,
+  sqrm: 120,
+  address: {
+    street: 'Happy St',
+    number: 123,
+    city: 'Miami',
+    state: 'FL',
+    country: 'US'
+  },
+  amenities: ['pool', 'tennis court', 'private parking', 'yard']
+};
+
+function checkProperty (obj) {
+  if (obj.isForSale === true) {
+    console.log(`The owner, ${obj.owner.firstName} put the home for sale! The property has ${obj.amenities.length} amenities.`);
+  } else {
+    console.log(`The home in ${obj.address.street} no.${obj.address.number} is not for sale`);
+  }
+}
+
+checkProperty(property);
   
 
